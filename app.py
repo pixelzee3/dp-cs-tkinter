d8243dc3a17f54717a5e2bdcce6b6eff4d1eadbe
PixelZee3 <bpixelzee@gmail.com>
2023-09-29 17:46:59 +0700

    Add GAA

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 from typing import Tuple, Dict
 from ctypes import windll
 from pages import Page
-from custom_pages import PageG, PageGA, TestPage1, TestPage2
+from custom_pages import PageG, PageGA, PageGAA, TestPage1, TestPage2
 
 # fix high DPI blurriness in Windows
 windll.shcore.SetProcessDpiAwareness(1)
@@ -20,7 +20,9 @@ class App(tk.Tk):
         self.pages: Dict[str, Page] = {
             'G': PageG(self, self.reset, self.select),
             'GA': PageGA(self, self.reset, self.select),
-            'GAA': TestPage1(self, self.reset, self.select),
+            'GAA': PageGAA(self, self.reset, self.select),
+            'GAAA': TestPage1(self, self.reset, self.select),
+            'GAAB1': TestPage2(self, self.reset, self.select),
             'GAB': TestPage2(self, self.reset, self.select),
             'GB': TestPage2(self, self.reset, self.select),
         }
--- custom_pages.py
+++ custom_pages.py
@@ -162,3 +162,65 @@ class PageGA(Page):
     def create_page(self, reset_function, select):
         ImageMain(self, 'images/GA.png')
         MenuGA(self, reset_function, select)
+
+# * GAA ============================================================================================
+
+class MenuGAA(Menu):
+    def __init__(self, parent, reset_function, select):
+        super().__init__(parent, reset_function, select)
+
+        # define grid
+        self.columnconfigure(0, weight=1, uniform='a')
+        self.rowconfigure((0, 1), weight=1, uniform='a')
+    
+    # if radio button is clicked, enable button
+    def on_radio_click(self):
+        if self.radio_var.get():
+            self.submit['state'] = 'enabled'
+        else:
+            self.submit['state'] = 'disabled'
+
+    # select pages according to selection
+    def on_submit(self, select):
+        if self.radio_var.get() == 'visit':
+            select('GAAA')
+        else:
+            select('GAAB1')
+
+    def create_widgets(self, select):
+        # define radio frame, radio variable, and submit button
+        self.radio = ttk.Frame(self)
+        self.radio_var = tk.StringVar()
+        self.submit = ttk.Button(self.radio, text='Submit', command=lambda: self.on_submit(select))
+
+        # disable button initially
+        self.submit['state'] = 'disabled'
+
+        # define radio buttons
+        self.radio_left = ttk.Radiobutton(self.radio, text="Visit the village", variable=self.radio_var, value="visit", command=self.on_radio_click)
+        self.radio_right = ttk.Radiobutton(self.radio, text="Pass the village", variable=self.radio_var, value="pass", command=self.on_radio_click)
+
+        # define text
+        self.label = ttk.Label(self, text='Billy drank enough water such that he was able to traverse the forest. Whilst doing so, he finds a village. Should he pass it or should he visit it?', font='Calibri 16')
+
+    def create_layout(self):
+        self.radio.columnconfigure((0, 1), weight=1, uniform='a')
+        self.radio.rowconfigure((0, 1), weight=1, uniform='a')
+
+        self.radio_left.grid(row=0, column=0)
+        self.radio_right.grid(row=0, column=1)
+        self.submit.grid(column=0, row=1, columnspan=2)
+
+        self.label.grid(column=0, row=0)
+        self.radio.grid(column=0, row=1)
+
+        # Make label wrap text dynamically according to width of frame
+        self.bind('<Configure>', lambda event: self.label.configure(wraplength=event.width))
+
+class PageGAA(Page):
+    def __init__(self, parent, reset: Callable, select: Callable):
+        super().__init__(parent, reset, select)
+    
+    def create_page(self, reset_function, select):
+        ImageMain(self, 'images/GAA.png')
+        MenuGAA(self, reset_function, select)
