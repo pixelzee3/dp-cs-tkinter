9035760f3deac6702742571b00441415945644d6
PixelZee3 <bpixelzee@gmail.com>
2023-09-29 17:59:00 +0700

    add GAAA

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 from typing import Tuple, Dict
 from ctypes import windll
 from pages import Page
-from custom_pages import PageG, PageGA, PageGAA, TestPage1, TestPage2
+from custom_pages import PageG, PageGA, PageGAA, PageGAAA, TestPage1, TestPage2
 
 # fix high DPI blurriness in Windows
 windll.shcore.SetProcessDpiAwareness(1)
@@ -21,7 +21,7 @@ class App(tk.Tk):
             'G': PageG(self, self.reset, self.select),
             'GA': PageGA(self, self.reset, self.select),
             'GAA': PageGAA(self, self.reset, self.select),
-            'GAAA': TestPage1(self, self.reset, self.select),
+            'GAAA': PageGAAA(self, self.reset, self.select),
             'GAAB1': TestPage2(self, self.reset, self.select),
             'GAB': TestPage2(self, self.reset, self.select),
             'GB': TestPage2(self, self.reset, self.select),
