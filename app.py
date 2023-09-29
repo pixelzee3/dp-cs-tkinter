ad4bb147e2a7a5e271211e3e32d294702ab2f71f
PixelZee3 <bpixelzee@gmail.com>
2023-09-29 19:16:02 +0700

    reimplement reset'

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 from typing import Tuple, Dict
 from ctypes import windll
 from pages import Page
-from custom_pages import PageG, PageGA, PageGAA, PageGAAA, PageGAAB1, TestPage1, TestPage2
+from custom_pages import PageG, PageGA, PageGAA, PageGAAA, PageGAAB1, PageGAAB2, TestPage1, TestPage2
 
 # fix high DPI blurriness in Windows
 windll.shcore.SetProcessDpiAwareness(1)
@@ -17,24 +17,30 @@ class App(tk.Tk):
         self.geometry(f'{geometry[0]}x{geometry[1]}')
         self.minsize(minsize[0], minsize[1])
         self.state('zoomed')
-        self.pages: Dict[str, Page] = {
-            'G': PageG(self, self.reset, self.select),
-            'GA': PageGA(self, self.reset, self.select),
-            'GAA': PageGAA(self, self.reset, self.select),
-            'GAAA': PageGAAA(self, self.reset, self.select),
-            'GAAB1': PageGAAB1(self, self.reset, self.select),
-            'GAB': TestPage2(self, self.reset, self.select),
-            'GB': TestPage2(self, self.reset, self.select),
+        self.page_config = {
+            'G': PageG,
+            'GA': PageGA,
+            'GAA': PageGAA,
+            'GAAA': PageGAAA,
+            'GAAB1': PageGAAB1,
+            'GAAB2': PageGAAB2,
+            'GAB': TestPage2,
+            'GB': TestPage2,
         }
 
+        # initialize
         self.reset()
 
         # run the app
         self.mainloop()
 
     def reset(self):
+        # reinitialize pages, or perform reinitialization of pages upon reset so that pages have fresh state
+        self.pages: Dict[str, Page] = {name: cls(self, self.reset, self.select) for name, cls in self.page_config.items()}
+
         # put genesis page on screen
         self.pages['G'].lift()
+
 
     def select(self, message: str):
         self.pages[message].lift()
